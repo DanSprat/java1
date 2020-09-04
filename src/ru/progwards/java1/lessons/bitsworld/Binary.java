bd8b6fd12dbfa7aab671763ed629bdf6d2c90f39
Daniel Popov <popovdaniil2000@gmail.com>
2020-09-05 01:57:01 +0300

    Class Binary Done

---
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -0,0 +1,23 @@
+package ru.progwards.java1.lessons.bitsworld;
+
+public class Binary {
+    private byte num;
+    public Binary(byte num){this.num=num;}
+    @Override
+    public String toString(){
+        String ret="";
+        byte b=1;
+        for (int i =0;i<8;i++)
+        {
+          if ((num & (b<<i)) !=0 )
+              ret = 1+ret;
+          else ret = 0+ret;
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Binary binary = new Binary((byte)15);
+        System.out.println(binary.toString());
+    }
+}
