5259689d250b7867f00e5dd99a51f83d68138094
Daniel Popov <popovdaniil2000@gmail.com>
2020-09-08 01:42:09 +0300

    Class ArrayInteger done

---
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -0,0 +1,65 @@
+package ru.progwards.java1.lessons.bigints;
+
+import java.math.BigInteger;
+
+public class ArrayInteger {
+    byte[] digits;
+    int LastIndex;
+    ArrayInteger(int n){
+        digits = new byte[n];
+    }
+    void fromInt(BigInteger value){
+        for (int i =0;value.compareTo(BigInteger.ZERO)!=0;++i){
+            digits[i] = value.mod(BigInteger.TEN).byteValue();
+            value = value.divide(BigInteger.TEN);
+            LastIndex=i;
+        }
+    }
+    BigInteger toInt(){
+        String str = "";
+        for (int i =0;i<=LastIndex;++i){
+            str = digits[i] + str;
+        }
+        return new BigInteger(str);
+    }
+    boolean add(ArrayInteger num){
+        int max = Integer.max(LastIndex,num.LastIndex);
+        if (max > digits.length-1){
+            for (int i =0;i<=LastIndex;++i)
+                digits[i]=0;
+            return false;
+        }
+        else
+            for(int i =0;i<max;++i){
+                digits[i+1] =(byte) (digits[i+1]+ ((num.digits[i] + digits[i])/10));
+                digits[i] = (byte) ((num.digits[i] + digits[i])%10);
+
+            }
+            if (max!=LastIndex){
+                digits[max+1] =(byte)((digits[max+1]+num.digits[max+1])/10);
+                digits[max] = (byte) ((num.digits[max] + digits[max])%10);
+
+                LastIndex++;
+                return true;
+            }
+            else if (num.digits[max] + digits[max] <10){
+                digits[max] = (byte) ((num.digits[max] + digits[max])%10);
+                return true;
+            }
+            else {
+                for (int i =0;i<=LastIndex;++i)
+                    digits[i]=0;
+                    return false;
+            }
+
+    }
+    public static void main(String[] args) {
+        ArrayInteger arrayInteger = new ArrayInteger(3);
+        ArrayInteger arrayInteger1 = new ArrayInteger(3);
+        arrayInteger.fromInt(new BigInteger("990"));
+        arrayInteger1.fromInt(new BigInteger("10"));
+        System.out.println(arrayInteger.add(arrayInteger1));
+        System.out.println(arrayInteger.toInt());
+
+    }
+}
