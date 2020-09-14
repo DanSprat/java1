6fb3c38e518600622ed10277c7fc81974e666f6f
Daniel Popov <popovdaniil2000@gmail.com>
2020-09-15 00:34:11 +0300

    Class Coder fixed #1

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -9,19 +9,16 @@ public class Coder {
     public static void codeFile(String inFileName, String outFileName, char[] code, String logName) throws IOException {
         try {
             FileReader fileReader = new FileReader(inFileName);
+            FileWriter fileWriter = new FileWriter(outFileName);
             try {
-                FileWriter fileWriter = new FileWriter(outFileName);
                 Scanner scanner = new Scanner(fileReader);
-                try {
-                    String str = scanner.toString();
-                    for (int i = 0; i < str.length(); ++i) {
-                        fileWriter.write(code[(int) str.charAt(i)]);
-                    }
-                } finally {
-                    fileWriter.close();
+                String str = scanner.toString();
+                for (int i = 0; i < str.length(); ++i) {
+                    fileWriter.write(code[(int) str.charAt(i)]);
                 }
             } finally {
                 fileReader.close();
+                fileWriter.close();
             }
         } catch (Exception e) {
             try {
