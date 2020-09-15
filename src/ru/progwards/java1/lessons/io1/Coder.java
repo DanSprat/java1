112f1742d08e800068c17363f28a0ba29f9b2e36
Daniel Popov <popovdaniil2000@gmail.com>
2020-09-15 19:49:50 +0300

    Class Coder fixed #19

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Coder {
-    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) throws IOException {
+    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
         FileReader fileReader;
         FileWriter fileWriter;
         try {
