0f424854ae8ceca274736ba5c17ac6f44d583652
Daniel Popov <popovdaniil2000@gmail.com>
2020-09-15 19:32:41 +0300

    Class Coder fixed #16

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -9,32 +9,29 @@ import java.util.Scanner;
 public class Coder {
     public static void codeFile(String inFileName, String outFileName, char[] code, String logName) throws IOException {
         FileReader fileReader = null;
-        FileWriter fileWriter =null;
+        FileWriter fileWriter = null;
         try {
             fileReader = new FileReader(inFileName);
             fileWriter = new FileWriter(outFileName);
-            try {
-                Scanner scanner = new Scanner(fileReader);
-                String str;
-                while (scanner.hasNextLine()) {
-                    str=scanner.nextLine();
-                    for (int i = 0; i < str.length(); ++i) {
-                        fileWriter.write(code[(int) str.charAt(i)]);
-                    }
-                }
-            } finally {
-                fileReader.close();
-                fileWriter.close();
+            Scanner scanner = new Scanner(fileReader);
+            String str;
+            while(scanner.hasNextLine()){
+                str=scanner.nextLine();
+                for(int i=0;i<str.length();i++)
+                fileWriter.write(code[(int)str.charAt(i)]);
             }
-        } catch (Exception e) {
-            FileWriter fileWriter2 = new FileWriter(logName);
+        } catch (Exception e){
+            FileWriter fileWriterLog = new FileWriter(logName);
             try {
-                fileWriter2.write(e.getMessage());
-            } catch (Exception e1) {
-                System.err.println(e1.getMessage());
+                fileWriterLog.write(e.getMessage());
+            } catch (Exception ex) {
+                System.err.println(ex.getMessage());
             } finally {
-                fileWriter2.close();
+                fileWriterLog.close();
             }
+        } finally {
+            fileReader.close();
+            fileWriter.close();
         }
     }
     public static void main(String[] args) throws IOException{
