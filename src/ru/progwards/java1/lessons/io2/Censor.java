fb012770e80e42c0d1ef0ed3c14c206355ffb9ab
Daniel Popov <popovdaniil2000@gmail.com>
2020-09-19 01:25:31 +0300

    Class Censor fixed #1

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -18,26 +18,24 @@ public class Censor {
          return str;
     }
     public static void censorFile(String inoutFileName, String[] obscene){
-
         try(RandomAccessFile randomAccessFile = new RandomAccessFile(inoutFileName,"rw")){
-         while (randomAccessFile.getFilePointer() <= randomAccessFile.length()){
+         while (randomAccessFile.getFilePointer() < randomAccessFile.length()){
              long start = randomAccessFile.getFilePointer();
-             if (start!=0){
-                 randomAccessFile.seek(randomAccessFile.getFilePointer()-1);
-                 int bc = randomAccessFile.read();
-                 while (!(bc ==0x0D || bc==0x0A)){
-                     bc=randomAccessFile.read();
-
+             if (start!=0){                                                        //Ставим курсор на начало строки
+                 randomAccessFile.seek(randomAccessFile.getFilePointer()-1); //Ставим курсор на начало строки
+                 int bc = randomAccessFile.read();                                //Ставим курсор на начало строки
+                 while (!(bc ==0x0D || bc==0x0A)){                                //Ставим курсор на начало строки
+                     bc=randomAccessFile.read();                                  //Ставим курсор на начало строки
                  }
-                 //randomAccessFile.seek(randomAccessFile.getFilePointer()+1);
              }
              String s = randomAccessFile.readLine();
              s = new String(s.getBytes("ISO-8859-1"),"UTF-8");
              for (int i=0;i<obscene.length;i++) {
                  if (s.contains(obscene[i])) {
-                     s = s.replace(obscene[i],setStars(obscene[i]));
+                     s = s.replace(obscene[i],setStars(obscene[i])); //SetStars заменяет строку из n символов на строку состоящую из n *
                  }
              }
+             if (randomAccessFile.getFilePointer()+1<randomAccessFile.length())
              s+='\n';
              byte [] b = s.getBytes("UTF-8");
              randomAccessFile.seek(start);
