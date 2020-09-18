8b9937bfba71df23a3cf0b29f77a49e926ed82e1
Daniel Popov <popovdaniil2000@gmail.com>
2020-09-18 20:28:07 +0300

    Class Translator fixed #1

--- src/ru/progwards/java1/lessons/io2/Translator.java
+++ src/ru/progwards/java1/lessons/io2/Translator.java
@@ -14,12 +14,17 @@ public class Translator {
         for (int i=0;i<inLang.length;i++){
             if(tmp.contains(inLang[i])){
                 if(Character.isUpperCase(sentence.charAt(tmp.indexOf(inLang[i])))){
-                   tmp.replace(inLang[i],outLang[i].replaceFirst(String.valueOf(outLang[i].charAt(0)),String.valueOf(Character.toUpperCase(outLang[i].charAt(0)))));
+                  tmp = tmp.replace(inLang[i],outLang[i].replaceFirst(String.valueOf(outLang[i].charAt(0)),String.valueOf(Character.toUpperCase(outLang[i].charAt(0)))));
                 } else {
-                    tmp.replace(inLang[i],outLang[i]);
+                    tmp = tmp.replace(inLang[i],outLang[i]);
                 }
             }
         }
         return tmp;
+    }
+
+    public static void main(String[] args) {
+        Translator translator = new Translator(new String[]{"make", "love", "not", "war"},new String[]{"твори", "любовь", "не", "войну"});
+        System.out.println(translator.translate("make love not war"));
     }
 }
