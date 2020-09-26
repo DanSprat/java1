ff9022be0871ea5d1f5cb2d8f8f9bc334bd06afa
Daniel Popov <popovdaniil2000@gmail.com>
2020-09-26 17:41:21 +0300

    Classes Order and OrderQueue modified #1

--- src/ru/progwards/java1/lessons/queues/Order.java
+++ src/ru/progwards/java1/lessons/queues/Order.java
@@ -3,7 +3,7 @@ package ru.progwards.java1.lessons.queues;
 import java.util.ArrayList;
 import java.util.Queue;
 
-public class Order {
+public class Order implements Comparable<Order> {
     private static int count = 0;
     private double sum;
     private int num;
@@ -23,4 +23,10 @@ public class Order {
         return num;
     }
 
+    @Override
+    public int compareTo(Order order) {
+        Integer a = Integer.compare(getQueueClass(),order.getQueueClass());
+        if (a!=0) return a;
+        else return Integer.compare(getNum(),order.getNum());
+    }
 }
--- src/ru/progwards/java1/lessons/queues/OrderQueue.java
+++ src/ru/progwards/java1/lessons/queues/OrderQueue.java
@@ -5,23 +5,18 @@ import java.util.*;
 public class OrderQueue {
     PriorityQueue<Order> priorityQueue;
     public OrderQueue() {
-        priorityQueue = new PriorityQueue<>(new Comparator<Order>() {
-            @Override
-            public int compare(Order order, Order t1) {
-                return Integer.compare(order.getQueueClass(),t1.getQueueClass());
-            }
-        });
+        priorityQueue = new PriorityQueue<>();
     }
     public void add(Order order){
         if (order.getSum() >20000){
             order.setQueueClass(1);
-            priorityQueue.offer(order);
+            priorityQueue.add(order);
         } else if (order.getSum() <=10000){
             order.setQueueClass(3);
-            priorityQueue.offer(order);
+            priorityQueue.add(order);
         } else {
             order.setQueueClass(2);
-            priorityQueue.offer(order);
+            priorityQueue.add(order);
         }
     }
     public Order get(){
@@ -31,18 +26,44 @@ public class OrderQueue {
     }
 
     public static void main(String[] args) {
+
         OrderQueue orderQueue = new OrderQueue();
-        orderQueue.add(new Order(1000));//3
-        orderQueue.add(new Order(200000));//1
-        orderQueue.add(new Order(30000));//1
-        orderQueue.add(new Order(400));//3
-        orderQueue.add(new Order(5000));//3
-        orderQueue.add(new Order(70000));//1
-        orderQueue.add(new Order(20000));//2
-        orderQueue.add(new Order(10000));//3
+        orderQueue.add(new Order(21617.0));//3
+        orderQueue.add(new Order(29057.0));//1
+        orderQueue.add(new Order(16492.0));//1
+        orderQueue.add(new Order( 26173.0));//3
+        orderQueue.add(new Order(3402.0));//3
+        orderQueue.add(new Order(10260.0));//1
+        orderQueue.add(new Order(11086.0));//2
+        orderQueue.add(new Order(15853.0));//3
+
+        orderQueue.add(new Order( 19271.0));//3
+        orderQueue.add(new Order(21984.0));//1
+        orderQueue.add(new Order(8242.0));//1
+        orderQueue.add(new Order( 17181.0));//3
+        orderQueue.add(new Order( 23126.0));//3
+        orderQueue.add(new Order( 5820.0));//1
+        orderQueue.add(new Order(7673.0));//2
+        orderQueue.add(new Order(16284.0));//3
+
+        orderQueue.add(new Order( 27622.0));//3
+        orderQueue.add(new Order(5398.0));//1
+        orderQueue.add(new Order( 2301.0));//1
+        orderQueue.add(new Order(  9119.0));//3
+        orderQueue.add(new Order( 10371.0));//3
+        orderQueue.add(new Order(  3348.0));//1
+        orderQueue.add(new Order(11991.0));//2
+
+
+
         Order order = orderQueue.get();
-        System.out.println(order.getSum());
-        System.out.println(order.getQueueClass());
+        int i =0;
+        while (order!=null) {
+            System.out.println(order.getSum());
+          //  System.out.println(order.getQueueClass());
+            order = orderQueue.get();
+            i++;
+        }
         System.out.println("End");
     }
 }
