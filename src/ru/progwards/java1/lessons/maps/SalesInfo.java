babd0572878224eb3a7f77558f7259c884705850
Daniel Popov <popovdaniil2000@gmail.com>
2020-09-29 01:12:48 +0300

    Class SalesInfo done

---
+++ src/ru/progwards/java1/lessons/maps/SalesInfo.java
@@ -0,0 +1,74 @@
+package ru.progwards.java1.lessons.maps;
+
+import java.io.FileReader;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class SalesInfo {
+    ArrayList<Sale> saleList;
+    private class Sale{
+        String name;
+        String item;
+        int count;
+        double price;
+
+        public Sale(String name, String item, int count, double price) {
+            this.name = name;
+            this.item = item;
+            this.count = count;
+            this.price = price;
+        }
+    }
+
+    public SalesInfo() {
+        this.saleList = new ArrayList<>();
+    }
+
+    public int loadOrders(String fileName)throws Exception{
+        FileReader fileReader = new FileReader(fileName);
+        Scanner scanner = new Scanner(fileReader);
+        int countOfLines =0;
+        while (scanner.hasNextLine()){
+            String s = scanner.nextLine();
+           s= s.replaceAll("\\s+"," ");
+            String [] strs = s.split(",");
+            if (strs.length==4){
+              try {
+                  int count = Integer.parseInt(strs[2].trim());
+                  double price = Double.parseDouble(strs[3].trim());
+                  saleList.add(new Sale(strs[0],strs[1],count,price));
+                  countOfLines++;
+              } catch(Exception e) {
+                  throw new Exception("IncorrectLine");
+                }
+            }
+        }
+        return countOfLines;
+    }
+    public Map<String, Double> getGoods(){
+        TreeMap<String,Double> goodsMap = new TreeMap<>();
+        for (Sale sale:saleList){
+            Double d = goodsMap.putIfAbsent(sale.item,sale.count*sale.price);
+            if (d!=null){
+                goodsMap.replace(sale.item,goodsMap.get(sale.item)+sale.count*sale.price);
+            }
+        }
+        return goodsMap;
+    }
+    public Map<String, AbstractMap.SimpleEntry<Double, Integer>> getCustomers(){
+        TreeMap<String,AbstractMap.SimpleEntry<Double, Integer>> entryTreeMap = new TreeMap<>();
+        for (Sale sale:saleList){
+            AbstractMap.SimpleEntry<Double, Integer> as = entryTreeMap.putIfAbsent(sale.name,new AbstractMap.SimpleEntry<>(sale.price*sale.count,sale.count));
+            if (as!=null){
+                entryTreeMap.replace(sale.name,new AbstractMap.SimpleEntry<>(entryTreeMap.get(sale.name).getKey()+sale.price*sale.count,entryTreeMap.get(sale.name).getValue()+sale.count));
+            }
+        }
+        return entryTreeMap;
+    }
+
+    public static void main(String[] args) throws Exception{
+       SalesInfo salesInfo = new SalesInfo();
+       salesInfo.loadOrders("s");
+       System.out.println(salesInfo.getCustomers());
+    }
+}
