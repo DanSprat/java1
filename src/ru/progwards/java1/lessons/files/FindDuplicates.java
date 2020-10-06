0fbabcd8f7405de8449de38a3136d4948c0db486
Daniel Popov <popovdaniil2000@gmail.com>
2020-10-07 02:10:07 +0300

    Class FindDuplicates fixed #1.1 (added comments)

--- src/ru/progwards/java1/lessons/files/FindDuplicates.java
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -12,24 +12,24 @@ public class FindDuplicates {
     private  static List<ArrayList<String>> splitSames(ArrayList<Path> arrayList) {
             List<ArrayList<String>> result = new ArrayList<>();
         try {
-            result.add(new ArrayList<>(List.of(arrayList.get(0).toString())));
-            for (int i = 1; i < arrayList.size(); i++) {
+            result.add(new ArrayList<>(List.of(arrayList.get(0).toString()))); // Добавляю первый файл по умолчанию тем самым инициализируя первую группу
+            for (int i = 1; i < arrayList.size(); i++) { // В цикле проверяю, есть ли совпадения по содержанию/размеру/дате среди текущих групп
                 boolean isAdd = false;
                 for (int j = 0; j < result.size(); j++) {
-                    if (Files.getLastModifiedTime(arrayList.get(i)).equals(Files.getLastModifiedTime(Paths.get(result.get(j).get(0)))) &&
-                            Files.size(arrayList.get(i)) == Files.size(Paths.get(result.get(j).get(0)))) {
+                    if (Files.getLastModifiedTime(arrayList.get(i)).equals(Files.getLastModifiedTime(Paths.get(result.get(j).get(0)))) && //Совпадение дат
+                            Files.size(arrayList.get(i)) == Files.size(Paths.get(result.get(j).get(0)))) { // Совпадение размеров
                         if (Files.readString(arrayList.get(i)).equals(Files.readString(Paths.get(result.get(j).get(0))))) {
-                            result.get(j).add(arrayList.get(i).toString());
+                            result.get(j).add(arrayList.get(i).toString()); // Если есть то добавляю в группу
                             isAdd = true;
                             break;
                         }
                     }
                 }
                 if (!isAdd) {
-                    result.add(new ArrayList<>(List.of(arrayList.get(i).toString())));
+                    result.add(new ArrayList<>(List.of(arrayList.get(i).toString()))); // Если нет, то создаю новую группу
                 }
             }
-            for (int i = 0; i < result.size(); i++) {
+            for (int i = 0; i < result.size(); i++) { // Удаляю все списки,размер которых равен единице
                 if (result.get(i).size() == 1) {
                     result.remove(i);
                     i--;
@@ -41,14 +41,13 @@ public class FindDuplicates {
         return result;
     }
     public static List<List<String>> findDuplicates(String startPath){
-
             final Path dir = Paths.get(startPath);
             HashMap<String, ArrayList<Path>> listHashMap = new HashMap<>();
         try {
             PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**");
             Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
                 @Override
-                public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+                public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) { //Получаю Map всех файлов с одинаковым названием
                     if (pathMatcher.matches(path)) {
                         if (listHashMap.containsKey(path.getFileName().toString())) {
                             listHashMap.get(path.getFileName().toString()).add(path.toAbsolutePath());
@@ -65,7 +64,7 @@ public class FindDuplicates {
         List<List<String>> result = new ArrayList<>();
         for (var it: listHashMap.entrySet()){
             if (it.getValue().size()>1){
-                result.addAll(splitSames(it.getValue()));
+                result.addAll(splitSames(it.getValue())); // разбиваю файлы с одинаковым названием на идентичные файлы
             }
         }
         return result;
