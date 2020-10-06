26fc9dfba9dbdc9c441d8bf170a22c61fdc969c2
Daniel Popov <popovdaniil2000@gmail.com>
2020-10-07 01:38:47 +0300

    Class FindDuplicates fixed #1

--- src/ru/progwards/java1/lessons/files/FindDuplicates.java
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -9,51 +9,59 @@ import java.util.HashMap;
 import java.util.List;
 
 public class FindDuplicates {
-    private  static List<ArrayList<String>> splitSames(ArrayList<Path> arrayList) throws Exception{
-        List<ArrayList<String>> result = new ArrayList<>();
-        result.add(new ArrayList<>(List.of(arrayList.get(0).toString())));
-        for (int i =1;i<arrayList.size();i++){
-            boolean isAdd = false;
-            for(int j =0;j<result.size();j++){
-             if (Files.getLastModifiedTime(arrayList.get(i)).equals(Files.getLastModifiedTime(Paths.get(result.get(j).get(0)))) &&
-                 Files.size(arrayList.get(i))==Files.size(Paths.get(result.get(j).get(0)))) {
-                 if (Files.readString(arrayList.get(i)).equals(Files.readString(Paths.get(result.get(j).get(0))))){
-                     result.get(j).add(arrayList.get(i).toString());
-                     isAdd = true;
-                     break;
-                 }
-             }
+    private  static List<ArrayList<String>> splitSames(ArrayList<Path> arrayList) {
+            List<ArrayList<String>> result = new ArrayList<>();
+        try {
+            result.add(new ArrayList<>(List.of(arrayList.get(0).toString())));
+            for (int i = 1; i < arrayList.size(); i++) {
+                boolean isAdd = false;
+                for (int j = 0; j < result.size(); j++) {
+                    if (Files.getLastModifiedTime(arrayList.get(i)).equals(Files.getLastModifiedTime(Paths.get(result.get(j).get(0)))) &&
+                            Files.size(arrayList.get(i)) == Files.size(Paths.get(result.get(j).get(0)))) {
+                        if (Files.readString(arrayList.get(i)).equals(Files.readString(Paths.get(result.get(j).get(0))))) {
+                            result.get(j).add(arrayList.get(i).toString());
+                            isAdd = true;
+                            break;
+                        }
+                    }
+                }
+                if (!isAdd) {
+                    result.add(new ArrayList<>(List.of(arrayList.get(i).toString())));
+                }
             }
-            if (!isAdd){
-                result.add(new ArrayList<>(List.of(arrayList.get(i).toString())));
+            for (int i = 0; i < result.size(); i++) {
+                if (result.get(i).size() == 1) {
+                    result.remove(i);
+                    i--;
+                }
             }
-        }
-        for (int i = 0;i<result.size();i++){
-            if (result.get(i).size()==1){
-                result.remove(i);
-                i--;
-            }
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
         }
         return result;
     }
-    public static List<List<String>> findDuplicates(String startPath)throws Exception{
-        final Path dir = Paths.get(startPath);
-        HashMap<String, ArrayList<Path>> listHashMap =new HashMap<>();
-        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**");
-        Files.walkFileTree(dir,new SimpleFileVisitor<Path>(){
-            @Override
-            public FileVisitResult visitFile(Path path, BasicFileAttributes attrs){
-                if (pathMatcher.matches(path)){
-                   if(listHashMap.containsKey(path.getFileName().toString())){
-                       listHashMap.get(path.getFileName().toString()).add(path.toAbsolutePath());
-                   } else {
-                       listHashMap.put(path.getFileName().toString(),new ArrayList<>(List.of(path.toAbsolutePath())));
-                   }
-                }
-                return FileVisitResult.CONTINUE;
-            }
-        });
+    public static List<List<String>> findDuplicates(String startPath){
 
+            final Path dir = Paths.get(startPath);
+            HashMap<String, ArrayList<Path>> listHashMap = new HashMap<>();
+        try {
+            PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**");
+            Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+                    if (pathMatcher.matches(path)) {
+                        if (listHashMap.containsKey(path.getFileName().toString())) {
+                            listHashMap.get(path.getFileName().toString()).add(path.toAbsolutePath());
+                        } else {
+                            listHashMap.put(path.getFileName().toString(), new ArrayList<>(List.of(path.toAbsolutePath())));
+                        }
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
         List<List<String>> result = new ArrayList<>();
         for (var it: listHashMap.entrySet()){
             if (it.getValue().size()>1){
@@ -63,7 +71,7 @@ public class FindDuplicates {
         return result;
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         final String path = "C:\\Users\\Work\\IdeaProjects\\Progwards\\test";
         System.out.println(findDuplicates(path));
     }
