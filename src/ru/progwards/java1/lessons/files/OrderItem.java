24c7719536f1a65e6c9df0d022c60819b4f461d5
Daniel Popov <popovdaniil2000@gmail.com>
2020-10-08 01:35:39 +0300

    Classes of Order's Cluster test3

--- src/ru/progwards/java1/lessons/files/OrderItem.java
+++ src/ru/progwards/java1/lessons/files/OrderItem.java
@@ -7,4 +7,10 @@ public class OrderItem {
 
    public OrderItem(){
    }
+
+    public OrderItem(String googsName, int count, double price) {
+        this.googsName = googsName;
+        this.count = count;
+        this.price = price;
+    }
 }
